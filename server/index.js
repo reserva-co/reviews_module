c2fe47325dc10dbb6fcdb26ce975ed9d17d05f28
Jesse Forrester <jesse.forrester@yahoo.com>
2019-08-23 20:19:59 -0700

    database seeding complete, get requests return correct data

--- server/index.js
+++ server/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+const db = require(__dirname + '/../database/mongohelper.js');
 
 app.use(express.static('public'))
+app.use(bodyParser.json({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(port, () => console.log('listening on port 3000'));
+
+
+app.get('/api/reviews/1', function (req, res) {
+  db.find(1, (reviews, err)=> {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('server log: ', reviews);
+      res.send(reviews);
+    }
+  })
+});
