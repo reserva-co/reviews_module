88149f0067e07ff8ca498adc2a9e32824f20f049
Jesse Forrester <jesse.forrester@yahoo.com>
2019-08-30 13:01:15 -0700

    added urls for pics hosted on S3, added 10 more users, and adjusted review count

--- database/mongohelper.js
+++ database/mongohelper.js
@@ -7,56 +7,66 @@ const faker = require('faker');
 mongoose.connect('mongodb://localhost/reserva', { useNewUrlParser: true });
 
 const users = [
-  { name: 'Robert', pic: 'https://picsum.photos/id/1/200' },
-  { name: 'Stannis', pic: 'https://picsum.photos/id/0/200' },
-  { name: 'Gendry', pic: 'https://picsum.photos/id/10/200' },
-  { name: 'Renly', pic: 'https://picsum.photos/id/100/200' },
-  { name: 'Brienne', pic: 'https://picsum.photos/id/1001/200' },
-  { name: 'Melisandre', pic: 'https://picsum.photos/id/1002/200' },
-  { name: 'Cersei', pic: 'https://picsum.photos/id/1003/200' },
-  { name: 'Jaimie', pic: 'https://picsum.photos/id/1004/200' },
-  { name: 'Tywin', pic: 'https://picsum.photos/id/1005/200' },
-  { name: 'Tyrion', pic: 'https://picsum.photos/id/1006/200' },
-  { name: 'Varys', pic: 'https://picsum.photos/id/101/200' },
-  { name: 'Tommen', pic: 'https://picsum.photos/id/1008/200' },
-  { name: 'Bronn', pic: 'https://picsum.photos/id/1009/200' },
-  { name: 'Petyr', pic: 'https://picsum.photos/id/1011/200' },
-  { name: 'Joffrey', pic: 'https://picsum.photos/id/1010/200' },
-  { name: 'Margaery', pic: 'https://picsum.photos/id/1012/200' },
-  { name: 'Sandor', pic: 'https://picsum.photos/id/1013/200' },
-  { name: 'Gregor', pic: 'https://picsum.photos/id/1014/200' },
-  { name: 'Aegon', pic: 'https://picsum.photos/id/1015/200' },
-  { name: 'Daenerys', pic: 'https://picsum.photos/id/1016/200' },
-  { name: 'Drogo', pic: 'https://picsum.photos/id/102/200' },
-  { name: 'Arya', pic: 'https://picsum.photos/id/1018/200' },
-  { name: 'Bran', pic: 'https://picsum.photos/id/1019/200' },
-  { name: 'Jon', pic: 'https://picsum.photos/id/1020/200' },
-  { name: 'Ned', pic: 'https://picsum.photos/id/1021/200' },
-  { name: 'Sansa', pic: 'https://picsum.photos/id/1022/200' },
-  { name: 'Robb', pic: 'https://picsum.photos/id/1023/200' },
-  { name: 'Catelyn', pic: 'https://picsum.photos/id/1024/200' },
-  { name: 'Asha', pic: 'https://picsum.photos/id/1025/200' },
-  { name: 'Theon', pic: 'https://picsum.photos/id/1026/200' },
-  { name: 'Ramsay', pic: 'https://picsum.photos/id/1027/200' },
-  { name: 'Benjen', pic: 'https://picsum.photos/id/1028/200' },
-  { name: 'Samwell', pic: 'https://picsum.photos/id/1029/200' },
-  { name: 'Tormund', pic: 'https://picsum.photos/id/106/200/200' },
-  { name: 'Davos', pic: 'https://picsum.photos/id/1031/200' },
-  { name: 'Eddard', pic: 'https://picsum.photos/id/1032/200' },
-  { name: 'Ellaria', pic: 'https://picsum.photos/id/1033/200' },
-  { name: 'Balon', pic: 'https://picsum.photos/id/104/200' },
-  { name: 'Jaqen', pic: 'https://picsum.photos/id/1035/200' },
-  { name: 'Lyanna', pic: 'https://picsum.photos/id/1036/200' },
-  { name: 'Jorah', pic: 'https://picsum.photos/id/1037/200' },
-  { name: 'Nymeria', pic: 'https://picsum.photos/id/1038/200' },
-  { name: 'Oberyn', pic: 'https://picsum.photos/id/1039/200' },
-  { name: 'Osha', pic: 'https://picsum.photos/id/1040/200' },
-  { name: 'Rhaegal', pic: 'https://picsum.photos/id/1041/200' },
-  { name: 'Rickard', pic: 'https://picsum.photos/id/1042/200' },
-  { name: 'Ghost', pic: 'https://picsum.photos/id/1043/200' },
-  { name: 'Lady', pic: 'https://picsum.photos/id/1044/200' },
-  { name: 'Edmure', pic: 'https://picsum.photos/id/1045/200' },
-  { name: 'Walder', pic: 'https://picsum.photos/id/107/200' },
+  { name: 'Robert', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/1.jpg' },
+  { name: 'Stannis', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/2.jpg' },
+  { name: 'Gendry', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/3.jpg' },
+  { name: 'Renly', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/4.jpg' },
+  { name: 'Brienne', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/5.jpg' },
+  { name: 'Melisandre', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/6.jpg' },
+  { name: 'Cersei', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/7.jpg' },
+  { name: 'Jaimie', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/8.jpg' },
+  { name: 'Tywin', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/9.jpg' },
+  { name: 'Tyrion', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/10.jpg' },
+  { name: 'Varys', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/11.jpg' },
+  { name: 'Tommen', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/12.jpg' },
+  { name: 'Bronn', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/13.jpg' },
+  { name: 'Petyr', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/14.jpg' },
+  { name: 'Joffrey', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/15.jpg' },
+  { name: 'Margaery', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/16.jpg' },
+  { name: 'Sandor', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/17.jpg' },
+  { name: 'Gregor', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/18.jpg' },
+  { name: 'Aegon', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/19.jpg' },
+  { name: 'Daenerys', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/20.jpg' },
+  { name: 'Drogo', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/21.jpg' },
+  { name: 'Arya', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/22.jpg' },
+  { name: 'Bran', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/23.jpg' },
+  { name: 'Jon', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/24.jpg' },
+  { name: 'Ned', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/25.jpg' },
+  { name: 'Sansa', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/26.jpg' },
+  { name: 'Robb', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/27.jpg' },
+  { name: 'Catelyn', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/28.jpg' },
+  { name: 'Asha', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/29.jpg' },
+  { name: 'Theon', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/30.jpg' },
+  { name: 'Ramsay', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/31.jpg' },
+  { name: 'Benjen', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/32.jpg' },
+  { name: 'Samwell', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/33.jpg' },
+  { name: 'Tormund', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/34.jpg' },
+  { name: 'Davos', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/35.jpg' },
+  { name: 'Eddard', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/36.jpg' },
+  { name: 'Ellaria', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/37.jpg' },
+  { name: 'Balon', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/38.jpg' },
+  { name: 'Jaqen', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/39.jpg' },
+  { name: 'Lyanna', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/40.jpg' },
+  { name: 'Jorah', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/41.jpg' },
+  { name: 'Nymeria', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/42.jpg' },
+  { name: 'Oberyn', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/43.jpg' },
+  { name: 'Osha', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/44.jpg' },
+  { name: 'Rhaegal', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/45.jpg' },
+  { name: 'Rickard', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/46.jpg' },
+  { name: 'Ghost', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/47.jpg' },
+  { name: 'Lady', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/48.jpg' },
+  { name: 'Edmure', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/49.jpg' },
+  { name: 'Walder', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/50.jpg' },
+  { name: 'Hodor', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/51.jpg' },
+  { name: 'Greywind', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/52.jpg' },
+  { name: 'Missandei', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/53.jpg' },
+  { name: 'Rickon', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/54.jpg' },
+  { name: 'Jeyne', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/55.jpg' },
+  { name: 'Roose', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/56.jpg' },
+  { name: 'Viserys', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/57.jpg' },
+  { name: 'Podrick', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/58.jpg' },
+  { name: 'Beric', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/59.jpg' },
+  { name: 'Ygritte', pic: 'https://reviewmodulepics.s3-us-west-2.amazonaws.com/60.jpg' },
 ];
 
 const years = [2016, 2017, 2018];
@@ -85,7 +95,7 @@ const Review = mongoose.model('Review', reviewSchema);
 
 const getRandomInt = (min, max) => Math.floor(Math.random() * max + min);
 
-const getUser = () => users[getRandomInt(0, 50)];
+const getUser = () => users[getRandomInt(0, 60)];
 
 const getDate = () => {
   const date = `${faker.date.month()} ${years[getRandomInt(0, 3)]}`;
@@ -98,7 +108,7 @@ const getText = () => {
 };
 
 const getScore = () => {
-  const score = getRandomInt(0, 6);
+  const score = getRandomInt(1, 6);
   return score;
 };
 
@@ -107,7 +117,7 @@ db.once('open', () => {
   console.log('Seeding data to MongoDB');
 
   for (let i = 1; i < 100; i += 1) {
-    const totalReviews = getRandomInt(6, 26);
+    const totalReviews = getRandomInt(6, 33);
     const host = getUser();
     const responses = Math.floor(totalReviews * 0.3);
     let responseCount = 0;
